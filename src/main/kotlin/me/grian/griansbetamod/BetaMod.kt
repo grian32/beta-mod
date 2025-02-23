eb898e4c592ed9562bce288761baacdb60f32d83
grian <grianguy32@gmail.com>
2025-02-23 12:36:16 +0200

    feat: remove init event and move class to it's own.

---
+++ src/main/kotlin/me/grian/griansbetamod/BetaMod.kt
@@ -0,0 +1,12 @@
+package me.grian.griansbetamod
+
+import net.modificationstation.stationapi.api.util.Namespace
+import org.apache.logging.log4j.Logger
+
+object BetaMod {
+    val NAMESPACE: Namespace = Namespace.of("griansbetamod")
+
+    val LOGGER: Logger = NAMESPACE.logger
+
+
+}
