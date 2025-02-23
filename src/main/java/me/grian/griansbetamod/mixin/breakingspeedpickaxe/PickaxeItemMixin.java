c3cca5c32cbdda01a45be3be60a0eedee261207a
grian <grianguy32@gmail.com>
2025-02-23 22:57:03 +0200

    refactor: removes unused imports, debug annot

--- src/main/java/me/grian/griansbetamod/mixin/breakingspeedpickaxe/PickaxeItemMixin.java
+++ src/main/java/me/grian/griansbetamod/mixin/breakingspeedpickaxe/PickaxeItemMixin.java
@@ -3,16 +3,9 @@ package me.grian.griansbetamod.mixin.breakingspeedpickaxe;
 import com.google.common.collect.ObjectArrays;
 import net.minecraft.block.Block;
 import net.minecraft.item.PickaxeItem;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Debug(export = true)
 @Mixin(PickaxeItem.class)
 public class PickaxeItemMixin {
     @Shadow
