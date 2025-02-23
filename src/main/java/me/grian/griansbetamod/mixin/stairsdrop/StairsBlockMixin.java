5fa193019516c10232d8c2d1a2edbc0bc3575704
grian <grianguy32@gmail.com>
2025-02-23 22:23:26 +0200

    chore: remove debug annot

--- src/main/java/me/grian/griansbetamod/mixin/stairsdrop/StairsBlockMixin.java
+++ src/main/java/me/grian/griansbetamod/mixin/stairsdrop/StairsBlockMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
-@Debug(export = true)
 @Mixin(StairsBlock.class)
 public abstract class StairsBlockMixin extends Block {
     @Shadow
