bb43e3870bee5a0e2eba203dca18c3f2b35e53dc
grian <grianguy32@gmail.com>
2025-02-25 15:27:08 +0200

    feat: config for leath boot trampling

--- src/main/java/me/grian/griansbetamod/config/Config.java
+++ src/main/java/me/grian/griansbetamod/config/Config.java
@@ -5,4 +5,7 @@ import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
 public class Config {
     @ConfigEntry(name = "Lapis gives Speed Boost", description = "Requires a restart to take effect & feature only currently works client-side")
     public Boolean lapisSpeedBoost = true;
+
+    @ConfigEntry(name = "Leather Boots no longer trample crops.", description = "Requires a restart to take effect")
+    public Boolean leatherBootsTrampleCrops = true;
 }
