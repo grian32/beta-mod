7edffee51f749bdc9ae8a0deed9c7fedb75ecc5a
grian <grianguy32@gmail.com>
2025-02-25 21:23:57 +0200

    refactor: remove unused variable in TextureListener

--- src/main/kotlin/me/grian/griansbetamod/TextureListener.kt
+++ src/main/kotlin/me/grian/griansbetamod/TextureListener.kt
@@ -5,15 +5,11 @@ import net.mine_diver.unsafeevents.listener.EventListener
 import net.modificationstation.stationapi.api.client.event.texture.TextureRegisterEvent
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases
 import net.modificationstation.stationapi.api.util.Namespace
-import kotlin.properties.Delegates
 
 object TextureListener {
     private val NAMESPACE: Namespace = Namespace.of("griansbetamod")
 
     private val LOGGER = NAMESPACE.logger
-
-    @JvmStatic
-    var redstoneBlockTexture by Delegates.notNull<Int>()
 
     @EventListener
     fun registerTextures(event: TextureRegisterEvent) {
