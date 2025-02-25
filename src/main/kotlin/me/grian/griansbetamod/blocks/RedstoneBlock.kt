a8b12c0f2518525cb0b35bdb1a5179cb54a936bd
grian <grianguy32@gmail.com>
2025-02-25 20:33:01 +0200

    fix: redstone block now drops itself and works with pickaxes

--- src/main/kotlin/me/grian/griansbetamod/blocks/RedstoneBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/RedstoneBlock.kt
@@ -1,10 +1,6 @@
 package me.grian.griansbetamod.blocks
 
-import me.grian.griansbetamod.TextureListener
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.minecraft.block.material.Material
-import net.minecraft.world.BlockView
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
 
