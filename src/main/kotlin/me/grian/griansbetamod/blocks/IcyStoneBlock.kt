aada1d06ac0671dc6b8560dda8be4fbfb47dfa7c
grian <grianguy32@gmail.com>
2025-02-28 14:57:21 +0200

    feat: icy stone block/textures

---
+++ src/main/kotlin/me/grian/griansbetamod/blocks/IcyStoneBlock.kt
@@ -0,0 +1,8 @@
+package me.grian.griansbetamod.blocks
+
+import net.minecraft.block.material.Material
+import net.modificationstation.stationapi.api.template.block.TemplateBlock
+import net.modificationstation.stationapi.api.util.Identifier
+
+class IcyStoneBlock(identifier: Identifier) : TemplateBlock(identifier, Material.STONE) {
+}
