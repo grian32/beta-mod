93386d1cfa85c941859450a0a47d83d2088b37db
grian <grianguy32@gmail.com>
2025-02-28 14:58:03 +0200

    feat: overworld chunk gen mixin to gen icy stone

---
+++ src/main/java/me/grian/griansbetamod/mixin/icystone/OverworldChunkGeneratorMixin.java
@@ -0,0 +1,51 @@
+package me.grian.griansbetamod.mixin.icystone;
+
+import me.grian.griansbetamod.BetaMod;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.chunk.OverworldChunkGenerator;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Debug(export=true)
+@Mixin(OverworldChunkGenerator.class)
+public class OverworldChunkGeneratorMixin {
+    @Shadow
+    private World world;
+
+    @Unique
+    private Biome biomeMod;
+
+    /**
+     * @author Telvarost
+     * @source <a href="https://github.com/telvarost/TelsTerrain-StationAPI/blob/997b7a33dc3b627750c6e124b67ed0cc576e0fbd/src/main/java/com/github/telvarost/telsterrain/mixin/OverworldChunkGeneratorMixin.java#L35">...</a>
+     */
+    @Inject(method = "buildTerrain", at = @At("HEAD"))
+    void buildTerrain(int chunkX, int chunkZ, byte[] blocks, Biome[] biomes, double[] temperatures, CallbackInfo ci) {
+        int var4 = chunkX * 16;
+        int var5 = chunkZ * 16;
+        biomeMod = this.world.method_1781().getBiome(var4, var5);
+    }
+
+    // https://discord.com/channels/397834523028488203/782730916396924968/1344424528561963020
+    @ModifyVariable(method = "buildTerrain", at = @At("STORE"), ordinal = 15)
+    private int injected(int value, int chunkX, int chunkZ, byte[] blocks, Biome[] biomes, double[] temperatures) {
+        if (value == Block.STONE.id && (biomeMod == Biome.TAIGA || biomeMod == Biome.TUNDRA)) {
+            return BetaMod.icyStone.id;
+        }
+        return value;
+    }
+
+    @ModifyVariable(method = "buildSurfaces", at = @At("STORE"), ordinal = 13)
+    private int injected(int value, int chunkX, int chunkZ, byte[] blocks, Biome[] biomes) {
+        if(value == BetaMod.icyStone.id){
+            return Block.STONE.id;
+        }
+        return value;
+    }
+}
