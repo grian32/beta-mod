025534390213bacc0ee2f9c2e46000a0f5c7b1af
grian <grianguy32@gmail.com>
2025-02-28 14:51:45 +0200

    refactor: remove useless if in mining listener

--- src/main/kotlin/me/grian/griansbetamod/TextureListener.kt
+++ src/main/kotlin/me/grian/griansbetamod/TextureListener.kt
@@ -1,10 +1,12 @@
 package me.grian.griansbetamod
 
+import me.grian.griansbetamod.config.Config
 import me.grian.griansbetamod.config.ConfigScreen
 import net.mine_diver.unsafeevents.listener.EventListener
 import net.modificationstation.stationapi.api.client.event.texture.TextureRegisterEvent
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases
 import net.modificationstation.stationapi.api.util.Namespace
+import javax.print.attribute.standard.MediaSize.NA
 
 object TextureListener {
     private val NAMESPACE: Namespace = Namespace.of("griansbetamod")
@@ -17,5 +19,6 @@ object TextureListener {
 
         if (ConfigScreen.config.enableRedstoneBlock) BetaMod.redstoneBlock.textureId = terrain.addTexture(NAMESPACE.id("block/block_redstone")).index
         if (ConfigScreen.config.leatherBootsTrampleCrops) BetaMod.grassyBoots.setTexture(NAMESPACE.id("item/grassy_boots"))
+        if (ConfigScreen.config.icyStone) BetaMod.icyStone.textureId = terrain.addTexture(NAMESPACE.id("block/icy_stone")).index
     }
 }
