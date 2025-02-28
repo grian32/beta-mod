f4e09ade7ad4acd030b4ffa6056e81663e422f25
grian <grianguy32@gmail.com>
2025-02-28 18:10:24 +0200

    feat: icy stone now drops icy cobble

--- src/main/kotlin/me/grian/griansbetamod/blocks/IcyStoneBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/IcyStoneBlock.kt
@@ -1,8 +1,13 @@
 package me.grian.griansbetamod.blocks
 
+import me.grian.griansbetamod.BetaMod
 import net.minecraft.block.material.Material
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
+import java.util.*
 
 class IcyStoneBlock(identifier: Identifier) : TemplateBlock(identifier, Material.STONE) {
+    override fun getDroppedItemId(blockMeta: Int, random: Random?): Int {
+        return BetaMod.icyCobblestone.id
+    }
 }
