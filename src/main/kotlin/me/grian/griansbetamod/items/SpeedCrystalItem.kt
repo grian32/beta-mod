6b246c8700342ba16b9b9dbce0e2aeda08685ed9
grian <grianguy32@gmail.com>
2025-04-02 20:29:47 +0300

    refactor: move lapis lazuli speed functionality to speed crystal

--- src/main/kotlin/me/grian/griansbetamod/items/SpeedCrystalItem.kt
+++ src/main/kotlin/me/grian/griansbetamod/items/SpeedCrystalItem.kt
@@ -1,8 +1,20 @@
 package me.grian.griansbetamod.items
 
+import me.grian.griansbetamod.config.ConfigScreen
+import me.grians.griansbetamod.mixininterfaces.IPlayerEntityMixin
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 import net.modificationstation.stationapi.api.template.item.TemplateItem
 import net.modificationstation.stationapi.api.util.Identifier
 
 class SpeedCrystalItem(identifier: Identifier) : TemplateItem(identifier) {
-
+    override fun use(stack: ItemStack?, world: World?, user: PlayerEntity?): ItemStack? {
+        if (ConfigScreen.config.lapisSpeedBoost) {
+            --stack!!.count
+            (user as IPlayerEntityMixin).`beta_mod$setSpeedBoostTicks`(1200)
+            return stack
+        }
+        return stack
+    }
 }
