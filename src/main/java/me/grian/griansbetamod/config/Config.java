dbc647798f6cc9a4d2296da0a4eb537829ce87e4
grian <grianguy32@gmail.com>
2025-04-03 02:13:20 +0300

    doc: change config entry name for speed crystals

--- src/main/java/me/grian/griansbetamod/config/Config.java
+++ src/main/java/me/grian/griansbetamod/config/Config.java
@@ -3,7 +3,7 @@ package me.grian.griansbetamod.config;
 import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
 
 public class Config {
-    @ConfigEntry(name = "Lapis gives Speed Boost", description = "Requires a restart to take effect & feature only currently works client-side")
+    @ConfigEntry(name = "Speed Crystals which give a speed boost", description = "Requires a restart to take effect & feature only currently works client-side")
     public Boolean lapisSpeedBoost = true;
 
     @ConfigEntry(name = "Players wearing Grassy Boots no longer trample crops.", description = "Requires a restart to take effect, grassy boots are also included in this config.")
