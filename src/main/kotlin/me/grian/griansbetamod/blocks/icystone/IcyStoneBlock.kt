17d1546f262176612e599b6a162903f617a3e541
grian <grianguy32@gmail.com>
2025-04-12 23:15:57 +0300

    revert: ktlint fmt

--- src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyStoneBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyStoneBlock.kt
@@ -4,13 +4,10 @@ import me.grian.griansbetamod.BetaMod
 import net.minecraft.block.material.Material
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
-import java.util.Random
+import java.util.*
 
-class IcyStoneBlock(
-    identifier: Identifier,
-) : TemplateBlock(identifier, Material.STONE) {
-    override fun getDroppedItemId(
-        blockMeta: Int,
-        random: Random?,
-    ): Int = BetaMod.icyCobblestone.id
+class IcyStoneBlock(identifier: Identifier) : TemplateBlock(identifier, Material.STONE) {
+    override fun getDroppedItemId(blockMeta: Int, random: Random?): Int {
+        return BetaMod.icyCobblestone.id
+    }
 }
