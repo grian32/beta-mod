4aef8a1d09f6a301ae3e6867a8dee30dbaeb3ebd
grian <grianguy32@gmail.com>
2025-04-12 23:14:11 +0300

    refactor: ktlint fmt

--- src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyCobblestoneBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyCobblestoneBlock.kt
@@ -4,5 +4,6 @@ import net.minecraft.block.material.Material
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
 
-class IcyCobblestoneBlock(identifier: Identifier) : TemplateBlock(identifier, Material.STONE) {
-}
+class IcyCobblestoneBlock(
+    identifier: Identifier,
+) : TemplateBlock(identifier, Material.STONE)
--- src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyStoneBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/icystone/IcyStoneBlock.kt
@@ -4,10 +4,13 @@ import me.grian.griansbetamod.BetaMod
 import net.minecraft.block.material.Material
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
-import java.util.*
+import java.util.Random
 
-class IcyStoneBlock(identifier: Identifier) : TemplateBlock(identifier, Material.STONE) {
-    override fun getDroppedItemId(blockMeta: Int, random: Random?): Int {
-        return BetaMod.icyCobblestone.id
-    }
+class IcyStoneBlock(
+    identifier: Identifier,
+) : TemplateBlock(identifier, Material.STONE) {
+    override fun getDroppedItemId(
+        blockMeta: Int,
+        random: Random?,
+    ): Int = BetaMod.icyCobblestone.id
 }
