be737ff5dc7ab1e19d5cca5a971c64545e08d69d
grian <grianguy32@gmail.com>
2025-04-27 00:27:33 +0300

    feat: enhancement tooltips :DDDD

--- src/main/kotlin/me/grian/griansbetamod/itemenhancements/Enhancement.kt
+++ src/main/kotlin/me/grian/griansbetamod/itemenhancements/Enhancement.kt
@@ -1,11 +1,14 @@
 package me.grian.griansbetamod.itemenhancements
 
+import net.modificationstation.stationapi.api.util.Formatting
+
 enum class Enhancement(
     val guiName: String,
-    val id: Int
+    val color: Formatting,
+    val id: Int,
 ) {
-    NONE(guiName = "", id = -1),
-    EXTRA_LOGS(guiName = "Extra Logs", id = 0);
+    NONE(guiName = "", color = Formatting.WHITE, id = -1),
+    EXTRA_LOGS(guiName = "Extra Logs", color = Formatting.GOLD, id = 0);
 
     companion object {
         fun getFromId(id: Int) = entries.find { it.id == id }
