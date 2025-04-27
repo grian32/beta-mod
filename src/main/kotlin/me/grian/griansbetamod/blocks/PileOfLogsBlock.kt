900c4789aa05cfe3cae7cae147308ea57b372409
grian <grianguy32@gmail.com>
2025-04-27 18:26:16 +0300

    feat: final texture/models & hitboxes

--- src/main/kotlin/me/grian/griansbetamod/blocks/PileOfLogsBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/PileOfLogsBlock.kt
@@ -1,9 +1,25 @@
 package me.grian.griansbetamod.blocks
 
 import net.minecraft.block.material.Material
+import net.minecraft.util.math.Box
+import net.minecraft.world.World
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
 
 class PileOfLogsBlock(identifier: Identifier) : TemplateBlock(identifier, Material.WOOD) {
+    override fun isFullCube(): Boolean = false
+    override fun isOpaque(): Boolean = false
 
+    override fun getCollisionShape(world: World?, x: Int, y: Int, z: Int): Box = getBox(x, y, z)
+    override fun getBoundingBox(world: World?, x: Int, y: Int, z: Int): Box = getBox(x, y, z)
+
+    private fun getBox(x: Int, y: Int, z: Int): Box =
+        Box.createCached(
+            x.toDouble(),
+            y.toDouble(),
+            z.toDouble(),
+            x.toDouble() + 1,
+            y.toDouble() + 0.75,
+            z.toDouble() + 1
+        )
 }
