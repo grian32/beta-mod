f4564a76b65c4fcde11b150f01e277fda399863e
grian <grianguy32@gmail.com>
2025-04-27 19:20:38 +0300

    feat: add rotated pile of logs

--- src/main/kotlin/me/grian/griansbetamod/blocks/PileOfLogsBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/PileOfLogsBlock.kt
@@ -1,12 +1,31 @@
 package me.grian.griansbetamod.blocks
 
+import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.util.math.Box
 import net.minecraft.world.World
+import net.modificationstation.stationapi.api.block.BlockState
+import net.modificationstation.stationapi.api.item.ItemPlacementContext
+import net.modificationstation.stationapi.api.state.StateManager
+import net.modificationstation.stationapi.api.state.property.BooleanProperty
 import net.modificationstation.stationapi.api.template.block.TemplateBlock
 import net.modificationstation.stationapi.api.util.Identifier
+import net.modificationstation.stationapi.api.util.math.Direction
+
 
 class PileOfLogsBlock(identifier: Identifier) : TemplateBlock(identifier, Material.WOOD) {
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
+        builder?.add(ROTATED)
+        super.appendProperties(builder)
+    }
+
+    override fun getPlacementState(context: ItemPlacementContext?): BlockState =
+        when (context?.horizontalPlayerFacing) {
+            Direction.EAST, Direction.WEST -> defaultState.with(ROTATED, false)
+            Direction.NORTH, Direction.SOUTH -> defaultState.with(ROTATED, true)
+            else -> defaultState.with(ROTATED, false) // null, down, up, shouldnt happen with this property
+        }
+
     override fun isFullCube(): Boolean = false
     override fun isOpaque(): Boolean = false
 
@@ -22,4 +41,10 @@ class PileOfLogsBlock(identifier: Identifier) : TemplateBlock(identifier, Materi
             y.toDouble() + 0.75,
             z.toDouble() + 1
         )
+
+    companion object {
+        // needs to be equal to: private static final
+        @JvmStatic
+        private val ROTATED: BooleanProperty = BooleanProperty.of("rotated")
+    }
 }
