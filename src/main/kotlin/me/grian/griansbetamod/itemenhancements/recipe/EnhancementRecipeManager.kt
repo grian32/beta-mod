121a39e74aefdf3958c6bcc4e57896af1e926a2b
grian <grianguy32@gmail.com>
2025-04-28 21:52:08 +0300

    fix: recipe finding for diff tiers

--- src/main/kotlin/me/grian/griansbetamod/itemenhancements/recipe/EnhancementRecipeManager.kt
+++ src/main/kotlin/me/grian/griansbetamod/itemenhancements/recipe/EnhancementRecipeManager.kt
@@ -1,5 +1,6 @@
 package me.grian.griansbetamod.itemenhancements.recipe
 
+import me.grian.griansbetamod.itemenhancements.getEnhancementTier
 import me.grian.griansbetamod.itemenhancements.setEnhancement
 import me.grian.griansbetamod.itemenhancements.setEnhancementTier
 import net.minecraft.inventory.CraftingInventory
@@ -27,7 +28,7 @@ object EnhancementRecipeManager {
 
 
     fun findRecipe(input: CraftingInventory) = recipes.find {
-        input.getStack(0).item::class.java == it.toolType.clazz && input.getStack(1).containsOther(it.ingredients)
+        input.getStack(0).item::class.java == it.toolType.clazz && input.getStack(1).containsOther(it.ingredients) && input.getStack(0).getEnhancementTier() < it.enhancementTier
     }
 
     private fun ItemStack.containsOther(other: ItemStack) = this.item.id == other.item.id && this.count >= other.count && this.damage == other.damage
