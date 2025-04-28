ea54d7c459c78ff3c7a015f77e140b1453e6eed6
grian <grianguy32@gmail.com>
2025-04-28 16:57:00 +0300

    feat: support multi item crafting for 2x2 recipes

--- src/main/java/me/grian/griansbetamod/mixin/stackcrafting/CraftingResultSlotMixin.java
+++ src/main/java/me/grian/griansbetamod/mixin/stackcrafting/CraftingResultSlotMixin.java
@@ -6,18 +6,17 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.CraftingRecipeManager;
 import net.minecraft.screen.slot.CraftingResultSlot;
 import net.modificationstation.stationapi.impl.recipe.StationShapedRecipe;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@Debug(export = true)
 @Mixin(CraftingResultSlot.class)
 public class CraftingResultSlotMixin {
 
@@ -54,11 +53,18 @@ public class CraftingResultSlotMixin {
         /**
          * TODO: normalize recipe to have the same layout as input for 2x2 recipes, cuz f.e the recipe for 2x2 will be diff from the placement in the input & loop below will fail :(
          */
+        // is 2x3 -> is 2x2
+        //        -> is 1x3
+        //        -> is 1x2
 
         List<ItemStack> foundRecipeGrid = Arrays.stream(foundRecipe.getGrid())
                 // if its null & u call right then it throws
                 .map(it -> it != null ? it.right().orElseThrow() : null)
                 .toList();
+
+        if (is2x2(foundRecipeGrid)) {
+            foundRecipeGrid = normalize2x2Recipe(this.input, foundRecipeGrid);
+        }
 
         for (int i = 0; i <= this.input.size() - 1; i++) {
             ItemStack inputStack = this.input.getStack(i);
@@ -74,5 +80,30 @@ public class CraftingResultSlotMixin {
 
         // cancels vanilla behaviour, will return early if no station shaped recipe is found so it's ok.
         ci.cancel();
+    }
+
+    @Unique
+    private boolean is2x2(List<ItemStack> lst) {
+        return lst.size() == 6 &&
+            (
+                (lst.get(0) == null && lst.get(3) == null) ||
+                (lst.get(2) == null && lst.get(5) == null)
+            );
+    }
+
+    @Unique
+    private List<ItemStack> normalize2x2Recipe(Inventory input, List<ItemStack> recipe) {
+        ArrayList<ItemStack> normalizedRecipe = new ArrayList<>(Collections.nCopies(input.size(), null));
+        Deque<ItemStack> recipeItems = recipe.stream().filter(Objects::nonNull).collect(Collectors.toCollection(ArrayDeque::new));
+
+        for (int i = 0; i <= input.size() - 1; i++) {
+            if (input.getStack(i) == null) continue;
+
+            if (input.getStack(i) != null) {
+                normalizedRecipe.set(i,     recipeItems.pop());
+            }
+        }
+
+        return normalizedRecipe;
     }
 }
