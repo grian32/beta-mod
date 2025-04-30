f01868c432f486cc8e7dcea01df725a3f01ec503
grian <grianguy32@gmail.com>
2025-04-30 18:57:41 +0300

    feat(docs): theme config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -62,8 +62,10 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: 'Grian\'s Beta Expansion',
       logo: {
