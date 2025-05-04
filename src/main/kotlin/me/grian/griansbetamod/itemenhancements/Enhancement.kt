cf5d72304117031623d137c5d855a99cb33713df
grian <grianguy32@gmail.com>
2025-05-04 18:49:40 +0300

    feat: define all pick/hoe enhancements

--- src/main/kotlin/me/grian/griansbetamod/itemenhancements/Enhancement.kt
+++ src/main/kotlin/me/grian/griansbetamod/itemenhancements/Enhancement.kt
@@ -8,10 +8,18 @@ enum class Enhancement(
     val id: Int,
     val hasTiers: Boolean = true
 ) {
+    // axe
     NONE(guiName = "", color = Formatting.WHITE, id = -1),
     EXTRA_LOGS(guiName = "Extra Logs", color = Formatting.GOLD, id = 0),
     RESIN(guiName = "Resin Harvest", color = Formatting.YELLOW, id = 1),
-    AXE_UNBREAKING(guiName = "Reinforced", color = Formatting.GRAY, id = 2);
+    AXE_UNBREAKING(guiName = "Reinforced", color = Formatting.GRAY, id = 2),
+
+    // pick
+    LAPIS_MINER(guiName = "Lapis Miner", color = Formatting.BLUE, id = 3),
+    STEADY_HAND(guiName = "Steady Hand", color = Formatting.AQUA, id = 4),
+
+    // hoe
+    REPLANTER(guiName = "Replanter", color = Formatting.GREEN, id = 5, false);
 
     companion object {
         fun getFromId(id: Int) = entries.find { it.id == id }
