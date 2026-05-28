eaa69931505215931ac10f9d359dc4f3d8cec7c2
grian32 <grianguy32@gmail.com>
2026-05-28 23:30:49 +0300

    fix: colored transp on nether glass
    issue: other renderlayer 1 stff doesnt render behind, fix idk

--- src/main/kotlin/me/grian/griansbetamod/blocks/NetherGlassBlock.kt
+++ src/main/kotlin/me/grian/griansbetamod/blocks/NetherGlassBlock.kt
@@ -7,7 +7,7 @@ import java.util.Random
 
 class NetherGlassBlock(identifier: Identifier) : TemplateTranslucentBlock(identifier, 0,Material.GLASS, false) {
     override fun getRenderLayer(): Int {
-        return 0
+        return 1
     }
 
     override fun getDroppedItemId(blockMeta: Int, random: Random?): Int {
