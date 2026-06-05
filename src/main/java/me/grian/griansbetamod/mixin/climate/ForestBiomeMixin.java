0bd30f423a91b98724a118b3522da382223c71cb
grian32 <grianguy32@gmail.com>
2026-06-05 21:20:50 +0300

    feat: add flowers generating around birch trees

---
+++ src/main/java/me/grian/griansbetamod/mixin/climate/ForestBiomeMixin.java
@@ -0,0 +1,19 @@
+package me.grian.griansbetamod.mixin.climate;
+
+import me.grian.griansbetamod.climate.FloweryBirchTreeFeature;
+import net.minecraft.world.biome.ForestBiome;
+import net.minecraft.world.gen.feature.Feature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(ForestBiome.class)
+public class ForestBiomeMixin {
+    @Inject(method = "getRandomTreeFeature", at=@At(value = "RETURN", ordinal = 0), cancellable = true)
+    void getRandomTreeFeature(Random random, CallbackInfoReturnable<Feature> cir) {
+            cir.setReturnValue(new FloweryBirchTreeFeature());
+    }
+}
---
+++ src/main/kotlin/me/grian/griansbetamod/climate/FloweryBirchTreeFeature.kt
@@ -0,0 +1,47 @@
+package me.grian.griansbetamod.climate
+
+import net.minecraft.block.Block
+import net.minecraft.world.World
+import net.minecraft.world.gen.feature.BirchTreeFeature
+import java.util.*
+
+class FloweryBirchTreeFeature : BirchTreeFeature() {
+    override fun generate(
+        world: World,
+        random: Random,
+        x: Int,
+        y: Int,
+        z: Int
+    ): Boolean {
+        if (super.generate(world, random, x, y, z)) {
+            var flowerAmount = random.nextInt(3, 6)
+            var attemptAmount = flowerAmount * 12
+
+            while (flowerAmount > 0 && attemptAmount > 0) {
+                attemptAmount--
+
+                val flowerX = x + random.nextInt(-3, 4)
+                val flowerZ = z + random.nextInt(-3, 4)
+                val flowerY = world.getTopY(flowerX, flowerZ)
+                val blockId = world.getBlockId(flowerX, flowerY, flowerZ)
+                if (
+                    (blockId == 0 || blockId == Block.GRASS.id) &&
+                    // block dandelion. can place was blocking me since it was checking for air and i wanna replace grass aswell
+                    canPlantOnTop(world, flowerX, flowerY-1, flowerZ)
+                ) {
+                    println("Generated flower for birch $flowerAmount")
+                    world.setBlockWithoutNotifyingNeighbors(flowerX, flowerY, flowerZ, Block.DANDELION.id)
+                    flowerAmount--
+                }
+            }
+
+            return true
+        }
+        return false
+    }
+
+    fun canPlantOnTop(world: World, x: Int, y: Int, z: Int): Boolean {
+        val id = world.getBlockId(x, y, z)
+        return id == Block.GRASS_BLOCK.id || id == Block.DIRT.id || id == Block.FARMLAND.id
+    }
+}
